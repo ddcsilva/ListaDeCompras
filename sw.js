d808fec78a1066e11507a8bddd00be374656d97d
Danilo Silva <danilo.silva@msn.com>
2025-04-25 18:48:04 -0300

    adiciona suporte a PWA com registro de Service Worker, atualiza metadados para iOS e melhora a gestão de cache com novas versões de arquivos CSS e JS

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'lista-compras-v1.2.0';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação de requisições
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retorna o recurso do cache se existir
+        if (response) {
+          return response;
+        }
+
+        // Caso contrário, busca na rede
+        return fetch(event.request)
+          .then((response) => {
+            // Verifica se a resposta é válida
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clona a resposta pois ela só pode ser usada uma vez
+            const responseToCache = response.clone();
+
+            // Adiciona a resposta ao cache
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+}); 
